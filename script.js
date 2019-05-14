089d92dacb302aac46b8c23bb030f25b7e6a486c
mdvonavi <mdvonavi@gmail.com>
2019-05-14 10:20:21 +0300

    ex1

---
+++ script.js
@@ -0,0 +1,18 @@
+var money = +(prompt("Ваш бюджет на месяц?", "10000"));
+var time = prompt("Введите дату в формате YYYY-MM-DD", "2019-01-01");
+var exp1 = prompt("Введите обязательную статью расходов в этом месяце", "Еда");
+var val1 = +(prompt("Во сколько обойдется?", "1000"));
+var exp2 = prompt("Введите обязательную статью расходов в этом месяце", "Аренда");
+var val2 = +(prompt("Во сколько обойдется?", "1000"));
+var appData = {
+    budget : money,
+    timeData : time,
+    expenses : {
+        exp1 : val1,
+        exp2 : val2
+    },
+    optionalExpenses : {},
+    income : {},
+    savings : false
+};
+alert("В день Вы можете потратить " + ((appData.budget - appData.expenses.exp1 - appData.expenses.exp2)/30).toFixed(2) + " рублей");
